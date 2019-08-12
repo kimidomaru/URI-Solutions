8c988d8a4f40178033364ef708765263d2978d4c
Luikson Silva <luikson97@hotmail.com>
2019-08-11 23:12:53 -0300

    Add files via upload

---
+++ Python/1001.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+a = input()
+b = input()
+x = int(a) + int(b)
+print("X = "+str(x))
---
+++ Python/1002.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+a = input()
+pi = 3.14159
+x = float(a) * float(a) * pi
+print("A={:.4f}".format(x))
